664010b05e28dbe4f41c6e51c030310bd217ed63
danielrosado <dj.rosado@alumnos.urjc.es>
2020-02-15 19:26:19 +0100

    Update main

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from MERToolProcessor import MERToolProcessor
 
 if __name__ == '__main__':
     processor_factory = MERToolProcessorFactory('corpus/input_corpus.txt')
-    processors = [processor_factory.create_processor(subclass) for subclass in MERToolProcessor.__subclasses__()]
-    for processor in processors:
+    for subclass in MERToolProcessor.__subclasses__():
+        processor = processor_factory.create_processor(subclass)
         processor.prepare_input()
         processor.process_input()
         processor.format_output()
